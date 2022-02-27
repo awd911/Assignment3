1d311c1ef9f7db8df764dac18e0ba4c2c64fc9ad
awd911 <tom.milton91@gmail.com>
2022-02-27 10:15:17 -0500

    First Commit
    Import Error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAOecakOo8mD5LDEJmRTyVB_dbjRHLv71g",
+    authDomain: "frontendassignment3.firebaseapp.com",
+    projectId: "frontendassignment3",
+    storageBucket: "frontendassignment3.appspot.com",
+    messagingSenderId: "160431291859",
+    appId: "1:160431291859:web:4f5825745fde90a2f38326",
+    measurementId: "G-GG0BG09PTK"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
